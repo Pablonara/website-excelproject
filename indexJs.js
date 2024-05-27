26eba955fbc463461b0fb66e8a5aef476937161b
Pablonara <pablonara@pablonara.com>
2024-05-27 15:42:21 -0700

    it works omg dsfsdfdsfsd

--- indexJs.js
+++ indexJs.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const textContainer = new SplitType('#textContainer')
+    // const textContainer = new SplitType('#textContainer')
 
-    gsap.to('.char', {
-        y: 0,
-        stagger: 0.05,
-        delay: 0.2,
-        duration: .1
-    })
+    // gsap.to('.char', {
+    //     y: 0,
+    //     stagger: 0.05,
+    //     delay: 0.2,
+    //     duration: .1
+    // })
+      // Select all spans within the .textappear class
+  const textSpans = document.querySelectorAll('.textappear span');
+  
+  // Animate each character
+  textSpans.forEach((span, index) => {
+    gsap.from(span, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      delay: index * 0.1, // Stagger the animation by 0.1s for each character
+      ease: "back.out(1.7)"
+    });
+  });
 });
+
 
